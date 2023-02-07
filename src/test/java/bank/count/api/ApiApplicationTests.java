6face4b03e4c83c61635b472661cd641c438a5ad
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-02-07 19:25:31 -0300

    Initialize project

---
+++ src/test/java/bank/count/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.count.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
